f66325db23cfb32555e63a3d18460abfadf82359
macbook <quanghoa.trinh@gmail.com>
2023-02-02 16:30:03 +0700

    first commit

---
+++ src/ThongTin.java
@@ -0,0 +1,19 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class ThongTin {
+
+    public TextField txtFullName;
+    public TextField txtEmail;
+    public Text txtInfo;
+
+    public void submit(ActionEvent actionEvent) {
+        String fn = txtFullName.getText();
+        String em = txtEmail.getText();
+        String txt = txtInfo.getText();
+        txt += "\n=================";
+        txt += "\nFullname: "+fn+"\n"+"Email: "+em;
+        txtInfo.setText(txt);
+    }
+}

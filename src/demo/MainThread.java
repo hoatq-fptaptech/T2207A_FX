89577e86ec9c26ef719d5aa56501baa0abeaa07d
macbook <quanghoa.trinh@gmail.com>
2023-02-27 16:31:04 +0700

    threads

--- src/demo/MainThread.java
+++ src/demo/MainThread.java
@@ -4,6 +4,30 @@ public class MainThread {
     public static void main(String[] args){
         Sub1 s1 = new Sub1(5,30);
         s1.start();
+
+        Sub2 s2 = new Sub2();
+        Thread t = new Thread(s2);
+        t.start();
+
+        Runnable r = ()->{
+            for(int i=0;i<20;i++){
+                System.out.println("Main i ="+i);
+                try {
+                    Thread.sleep(1000);
+                }catch (Exception e){
+
+                }
+            }
+        };
+
+        Thread t2 = new Thread(r);
+        t2.start();
+
+        // tao thread chay luon ma ko can khai bao bien
+        new Thread(()->{
+           // can chay gi viet vao day
+        }).start();
+
 //        for(int i=0;i<20;i++){
 //            System.out.println("Main i ="+i);
 //            try {
---
+++ src/demo/Sub2.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class Sub2 implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i=0;i<20;i++){
+            System.out.println("Sub 2 i ="+i);
+            try {
+                Thread.sleep(1000);
+            }catch (Exception e){
+
+            }
+        }
+    }
+}
